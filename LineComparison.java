7b3190587c03668066ba6fd879da6e29d07e75e6
unknown <piyushmishra.p@gmail.com>
2022-04-09 02:12:27 +0530

    uc 1 intoduction

---
+++ LineComparison.java
@@ -0,0 +1,18 @@
+package line_comparison;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
+import java.util.Scanner;
+
+public class LineComparison {
+    static Logger logger = Logger.getLogger(LineComparison.class);
+    public static void main(String [] args){
+        BasicConfigurator.configure();
+        logger.info("welcome to Line Comparison Computation on Master branch  ");
+
+
+
+    }
+
+}
