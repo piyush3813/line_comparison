a0654ec7609cfb1bf523bd83da0204a24d91b05d
unknown <piyushmishra.p@gmail.com>
2022-04-09 11:32:08 +0530

    checking the length of line

--- LineComparison.java
+++ LineComparison.java
@@ -10,6 +10,19 @@ public class LineComparison {
     public static void main(String [] args){
         BasicConfigurator.configure();
         logger.info("welcome to Line Comparison Computation on Master branch  ");
+        Scanner sc = new Scanner(System.in);
+        logger.info("enter the x1");
+        double x1 = sc.nextDouble();
+        logger.info("enter y1");
+        double y1 = sc.nextDouble();
+        logger.info("enter the x2");
+        double x2 = sc.nextDouble();
+        logger.info("enter y2");
+        double y2 = sc.nextDouble();
+        double z1=x1-x2;
+        double z2=y1-y2;
+        double length = Math.sqrt(Math.pow(z1,2)+Math.pow(z2,2));
+        logger.info("length of line = "+length);
 
 
 
